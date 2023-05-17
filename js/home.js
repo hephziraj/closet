bf189b3ce87f5f092dc0c78d956881874531e35e
hephziraj <rajhephzi4@gmail.com>
2023-05-17 14:50:47 +0530

    first commit

---
+++ js/home.js
@@ -0,0 +1,6 @@
+//  function "on_hover()"{
+//     // alert('Im Working')
+//     // document.getElementById("hide").style.display="block"
+//     document.getElementById("hide").style.display = "block";
+
+//  }
